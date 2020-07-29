7cc74430ea0061a29ba61e54cee0fd2dd11e4489
Felipe Dolago <lagofelipe@gmail.com>
2020-07-29 15:16:10 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ row_index = 0
 while row_index < src.count do
 element_index = 0
 while element_index < src[row_index].count do
-if scr[row_index][element_index].str?
+if scr[row_index][element_index].Kind_of(String)
   strings_reduction += scr[row_index][element_index]
 end
 element_index += 1
